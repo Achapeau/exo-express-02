3b7c25b08e879c1f157d04b7dcfc228b46dee1a6
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-07-24 10:10:28 +0200

    update exercice express 03

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -12,9 +14,15 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const userHandlers = require("./userHandlers");
+const movieHandlers = require('./movieHandlers')
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
+
+app.post('/api/movies', movieHandlers.postMovie);
+app.post('/api/users', userHandlers.postUser)
 
 app.listen(port, (err) => {
   if (err) {
